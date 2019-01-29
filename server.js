d993d878aa7fe6b4db8557b3f4b0a67e6ec77a3f
vishal7581 <vishal.chauhan7581@gmail.com>
2019-01-29 23:03:49 +1100

    initial

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 var app = express();
 
 
@@ -55,6 +56,6 @@ app.get('/bad',(req,res) => {
 });
 
 
-app.listen(3000,()=>{
-  console.log('server is running at port 3000');
+app.listen(port,()=>{
+  console.log(`server is running on ${port}`);
 });
